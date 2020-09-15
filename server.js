87a09509afb5ba52c552e108f68e9a1c7d377bc6
andrewjspivey <12spivey@gmail.com>
2020-09-15 15:52:36 -0700

    adds base server congiguration

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+
+// External Modules
 const express = require("express");
+// Instanced Modules
 const app = express();
+//configuration
 const PORT = 4000;
+
+
+
+
+
+
+//server listener
+app.listen(PORT, function () { // server listener
+    console.log(`Server is live and listening at ${PORT}`)
+});
